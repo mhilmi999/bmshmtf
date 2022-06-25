cdfdebc98d286f3835a136026d5b77266bc396de
mhilmi999 <hilmi.muhammad.rmdhn@gmail.com>
2022-06-25 11:32:51 +0700

    Fix

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -9,6 +9,10 @@
     <!-- bootstrap template will be here -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <!-- bootstrap template will be here -->
+    
+    <!-- Little icon -->
+    <link href="https://arek.its.ac.id/hmtf/wp-content/uploads/2020/07/logo-hmtf-its-Small.png" rel="icon">
+    <!-- Little icon -->
 
     <!-- script here -->
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
